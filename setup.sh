551bc074cad1b181a70deae4cb8375aae1520a19
sankeerth bussa <sankeerthbussa@gmail.com>
2022-01-08 21:54:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum install -y git
+sudo git clone https://github.com/sankeerth-bussa/diabetes_mlops.git
 sudo yum -y install terraform
 
 sudo yum install docker -y
